ef3062fe41578278f668888eafac4630998d9c68
tszfuichu <tszfuichu@gmail.com>
2026-03-16 16:50:17 +0800

    Add configuration settings in config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+        'total_hr_candle' : 720,
+        'run_interval_second': 3600,
+        'assets': ["BTC/USDT", "ETH/USDT"],
+        'account_balance': 10000.0,
+        'risk_pct': 0.05,
+        'z_entry_threshold': 2.0,
+        'z_stop_threshold': 4.0,
+    }
