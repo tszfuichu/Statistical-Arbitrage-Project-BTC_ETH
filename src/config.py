85d8e6a4d3cbc82bffad3def660ff9f0f9f5d275
tszfuichu <tszfuichu@gmail.com>
2026-03-17 16:59:37 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,9 @@ CONFIG = {
         'run_interval_second': 3600,
         'assets': ["BTC/USDT", "ETH/USDT"],
         'account_balance': 10000.0,
-        'risk_pct': 0.05,
+        'risk_pct': 0.1,
         'z_entry_threshold': 2.0,
         'z_stop_threshold': 4.0,
+        'fee_rate': 0.0002, # 0.02% futures taker fee per leg
+        'leverage': 20 #The default leverage in Binance Futures is 20x
     }
